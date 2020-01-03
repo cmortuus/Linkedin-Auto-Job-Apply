01ac298a3b344d6892ba63dbcdada9729a8f5a54
Catalyze326 <cfmcool@gmail.com>
2020-01-03 17:59:58 -0500

    Linkedin still hates scrapers

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ pip3 install selenium
 Once you have that in your path and you have installed selenium and you answer its questions you should be able to let it run.
 
 I got bored of applying to jobs so I wrote a program to do it for you. It simply asks for your username, password, work experience level, and filters in a text file and then references them wen looking through sites. I might slow it down so that Linkedin does not become suspicious, but right now I think it works.
+
+## How to use
+
+Things that will help. Set your career interests in your linkedin account. It will make it a lot easier to find the right jobs for you.
--- src/com/linkedin/main.py
+++ src/com/linkedin/main.py
@@ -1,93 +1,87 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchWindowException
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
 
 import re
-import os
 import time
+
 emailPattern = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 experanceLevelPattern = re.compile("^[JjMmSsLl]")
 driver = webdriver.Firefox()
 data = {}
-f = open('log', 'a+')
-f.write('\n\n\n\n')
+
+username = "mortoncf17@gcc.edu"
+passwd = "1Xd$6dnQsVli$4WBR*az9S*we03#BzHglqYQGEVc$th4vi74^c^dNpa9k"
+
+searchTerm = "automotive engineer"
+searchArea = "Worldwide"
 
 
-def getUserData():
-    if not os.path.isfile('metadata'):
-        f = open("metadata", "w+")
-        email = input('What is the email associated with your Linkedin account?')
-        while not emailPattern.match(email):
-            email = input('That is not a valid email address')
-        f.write(email + '\n')
-
-        password = input(
-            'What is the password you use to log into Linkedin. Note this is stored in the metadata file and not '
-            'put anywhere else.\nFeel free to valadate that.')
-        f.write(password + '\n')
-        return open('metadata', 'r').readlines()
-    else:
-        return open('metadata', 'r').readlines()
+wordsToAvoid = ["senior", "mastors"]
 
 
-def login():
-    driver.get('https://www.linkedin.com/login')
-    driver.find_element_by_id('username').clear()
-    driver.find_element_by_id('username').send_keys(data[0][:-1])
-    time.sleep(1)
-    driver.find_element_by_id('password').clear()
-    driver.find_element_by_id('password').send_keys(data[1][:-1])
-    driver.find_element_by_class_name('btn__primary--large').click()
+def scrape():
+    driver.get("https://www.linkedin.com/")
+    driver.find_element_by_class_name("nav__button-secondary").click()
 
+    uNameBar = driver.find_element_by_id("username")
+    uNameBar.clear()
+    uNameBar.send_keys(username)
 
-# TODO close other links
-def recursiveApply(linkNum = 0):
+    time.sleep(1.67)
+
+    passwdBar = driver.find_element_by_id("password")
+    passwdBar.clear()
+    passwdBar.send_keys(passwd)
+
+    time.sleep(1.67)
+
+    passwdBar.submit()
+
+    WebDriverWait(driver, 100).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "ember-view")))
+
+    driver.get("https://www.linkedin.com/jobs")
+
+    WebDriverWait(driver, 100).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "jobs-search-box__text-input")))
+
+    searchBar = driver.find_element_by_class_name("jobs-search-box__text-input")
+
+    searchBar.clear()
+    searchBar.send_keys(searchTerm)
+
+    searchBarArea = driver.find_element_by_xpath('//input[contains(@id, "jobs-search-box-location-id-ember")]')
+
+    searchBarArea.clear()
+    searchBarArea.send_keys(searchArea)
+
+    driver.find_element_by_class_name("jobs-search-box__submit-button").click()
+
+    time.sleep(4)
+
+    options = driver.find_elements_by_class_name("search-s-facet__form")
+    options[1].click()
+    driver.find_element_by_xpath('//label[@for="f_LF-f_AL"]').click()
+
+    jobList = driver.find_element_by_class_name("jobs-search-results__list").find_elements_by_tag_name("li")
+    jobList[0].find_element_by_class_name('job-card-search__time-badge').click()
     time.sleep(5)
-    if linkNum != 10:
-        # TODO get back to the jobs listing without going back to page 0
-        # TODO allow for senior if there is junior as well
-        # TODO this is the class for the title "fc-black-900"
-        driver.get('https://www.linkedin.com/jobs/')
-        # driver.find_element_by_class_name('test-pagination-next').click()
-        site = driver.find_elements_by_class_name('job-card__link-wrapper')[linkNum + 5]
+    jobList = driver.find_element_by_class_name("jobs-search-results__list").find_elements_by_tag_name("li")
+
+    for job in jobList:
         try:
-            site.click()
-        except ElementClickInterceptedException:
-            print('It failed to click on a new job link')
-            f.write('It failed to click on a new job link\n')
-            driver.get('https://www.linkedin.com/jobs/')
-            recursiveApply(linkNum + 1)
-        # jobInfo = driver.find_element_by_tag_name('body').text
-        # moveOn = False
-        #
-        # # Things you want in every posting
-        # for qualitiy in data[2].split(','):
-        #     if qualitiy.replace(' ', '') in jobInfo:
-        #         moveOn = True
-        # # Things to avoid in a posting
-        # for qualitiy in data[3].split(','):
-        #     if qualitiy.replace(' ', '') in jobInfo:
-        #         moveOn = False
-        # if not moveOn:
-        #     print('The listing did not contain the experience level preference of the user')
-        #     f.write('The listing did not contain the experience level preference of the user')
-        #     driver.get('https://www.linkedin.com/jobs/')
-        #     recursiveApply(linkNum + 1)
-        try:
-            driver.find_element_by_xpath('xpath=//').click()
-            driver.find_element_by_class_name('continue-btn').click()
-            print('We applied to a job')
-            f.write('We applied to a job')
-            recursiveApply(linkNum + 1)
-        except NoSuchElementException:
-            print('It was not a proper form easy apply site')
-            f.write('It was not a proper form easy apply site\n')
-            time.sleep(1.123)
-            recursiveApply(linkNum + 1)
-    else:
-        driver.get('https://www.linkedin.com/jobs/')
-        recursiveApply(0)
+            WebDriverWait(driver, 100).until(
+                expected_conditions.element_to_be_clickable((By.CLASS_NAME, "jobs-apply-button")))
+            job.find_element_by_class_name("jobs-apply-button").click()
+            for word in wordsToAvoid:
+                if word not in driver.find_element_by_id("job-details").text: break
+            print("This is a valid job to apply to")
+        except ElementClickInterceptedException as e:
+            print(e)
+        except NoSuchElementException as e:
+            print(e)
 
 
-data = getUserData()
-login()
-recursiveApply()
+scrape()
